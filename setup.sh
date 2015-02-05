6add42da34c03f78e08fd41ddc2cbe3d1aad52b4
Zhenhuan <lightmark@gmail.com>
2015-02-04 23:27:24 -0600

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,13 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup.
 
-ln -sb dotfiles/.tmux.conf .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sf dotfiles/.emacs.d .
+DEV_ROOT=`pwd`
+ln -sb $DEV_ROOT/dotfiles/.tmux.conf ~
+ln -sb $DEV_ROOT/dotfiles/.bash_profile ~
+ln -sb $DEV_ROOT/dotfiles/.bashrc ~
+
+if [ -d ~/.emacs.d/ ]; then
+        mv ~/.emacs.d ~/.emacs.d~
+fi
+ln -sf $DEV_ROOT/.emacs.d ~
+
